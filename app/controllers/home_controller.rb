4d942860553189cf2eda01b33e99f407d7bc135f
Alexey V. Pasenko <apasenko@gmail.com>
2017-09-28 14:01:16 +0300

    Сводная страница файлов и 440 положение

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -30,6 +30,76 @@ class HomeController < ApplicationController
                                 ORDER BY TIME_ DESC")
   end
 
+  def status_report
+    @date = params[:date].nil? ? DateTime.now.to_date : params[:date]
+    @f400_out = @client.sql.execute("SELECT f.name name
+                                          , CONVERT(char(10), f.time, 108) time
+                                          , r_file.name izvtub
+                                          , CONVERT(char(10), r_file.time, 108) time_izvtub
+                                    FROM fsmonitor.dbo.tfiles f
+                                      left join fsmonitor.dbo.trelations r on r.tfile_id_parent = f.id and r.type = 'izvtub'
+                                      left join fsmonitor.dbo.tfiles r_file on r_file.id = r.tfile_id
+                                    where CAST(f.date AS DATE) = CAST('#{@date}' AS DATE)
+                                            and f.name like 'AF_[_]3510123_MIFNS00%.arj'
+                                    order by f.id asc")
+    client2 = Elodb.new
+    @f400_in = client2.sql.execute("SELECT f.name name
+                                          , CONVERT(char(10), f.time, 108) time
+                                    FROM fsmonitor.dbo.tfiles f
+                                    where CAST(f.date AS DATE) = CAST('#{@date}' AS DATE)
+                                            and f.name like 'AF_[_]MIFNS00_3510123%.arj'
+                                    order by f.id asc")
+    client3 = Elodb.new
+    counts = client3.sql.execute("SELECT ISNULL((SELECT count(*) count
+                                  FROM FSMonitor.dbo.tfiles
+                                  where (name like '____3510123[_]%.vrb' or name like 'KWTFCB%.xml')
+                                        and CAST(date AS date) = CAST('#{@date}' AS DATE)
+                                  group by date), 0) as count;
+
+                                  SELECT ISNULL((SELECT count(*) count
+                                  FROM FSMonitor.dbo.tfiles
+                                  where (name like '____[_]____3510123[_]%.vrb' or name like 'PB_[_]%.xml')
+                                        and CAST(date AS DATE) = CAST('#{@date}' AS DATE)
+                                  group by date), 0) as count;
+
+                                  SELECT ISNULL((SELECT count(*) count
+                                  FROM FSMonitor.dbo.tfiles
+                                  where name like 'AF_[_]MIFNS00[_]3510123[_]%.arj'
+                                        and CAST(date AS DATE) = CAST('#{@date}' AS DATE)
+                                  group by date), 0) as count;
+
+                                  SELECT ISNULL((SELECT count(*) count
+                                  FROM FSMonitor.dbo.tfiles
+                                  where name like 'AF_[_]3510123[_]MIFNS00[_]%.arj'
+                                        and CAST(date AS DATE) = CAST('#{@date}' AS DATE)
+                                  group by date), 0) as count;
+
+                                  SELECT ISNULL((SELECT count(*) count
+                                  FROM elodb.dbo.elo_arh_post f
+                                  where f.filetype = 'ИЭС2' and f.posttype = 'mz' and CAST(f.dt AS DATE) = CAST('#{@date}' AS DATE)
+                                  group by f.filetype), 0) as count;
+
+                                  SELECT ISNULL((SELECT count(*) count
+                                  FROM elodb.dbo.elo_arh_post f
+                                  where f.filetype = 'ИЭС1' and f.posttype = 'mz' and CAST(f.dt AS DATE) = CAST('#{@date}' AS DATE)
+                                  group by f.filetype), 0) as count;
+
+                                  SELECT ISNULL((SELECT count(*) count
+                                  FROM elodb.dbo.elo_arh_post f
+                                  where f.filetype = 'ОЭС' and f.posttype = 'mz' and CAST(f.dt AS DATE) = CAST('#{@date}' AS DATE)
+                                  group by f.filetype), 0) as count;")
+    @f400_count_in,
+    @f400_count_out,
+    @f400_count_arj_in,
+    @f400_count_arj_out,
+    @f400_count_ptkpsd_in,
+    @f400_count_ptkpsd_in_error,
+    @f400_count_ptkpsd_out =
+        counts.collect { |c| c["count"] }
+    cmd = "call c:\\utils\\files_count.cmd #{@date}"
+    @f400_count_arj_system_in, @f400_count_arj_system_out = (%x( #{cmd} )).split(',')
+  end
+
   private
 
   def set_client
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
 
   get '/actions', to: 'home#history', as: 'user_history'
+  get '/status_report', to: 'home#status_report', as: 'report'
 
   post 'home/show_history'
 end
