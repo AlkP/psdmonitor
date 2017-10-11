019ee89e5c576a225361b35d725c229e35b61c0f
Alexey V. Pasenko <apasenko@gmail.com>
2017-10-11 17:05:54 +0300

    311 положение

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -144,6 +144,69 @@ class HomeController < ApplicationController
                                   FROM elodb.dbo.elo_arh_post f
                                   where f.filetype = N'ОЭС' and f.filename like 'wz___123.010%' and f.posttype = 'wz' and CAST(f.dt AS DATE) = CAST('#{@date}' AS DATE)
                                   group by f.filetype), 0) as count;")
+    client7 = Elodb.new
+    @f311_out = client7.sql.execute("SELECT f.name name
+                                          , CONVERT(char(10), f.time, 108) time
+                                          , r_file.name kvit
+                                          , CONVERT(char(10), r_file.time, 108) time_kvit
+                                          , r_file.success kvit_success
+                                    FROM fsmonitor.dbo.tfiles f
+                                      left join fsmonitor.dbo.trelations r on r.tfile_id_parent = f.id and r.type = '311_uvArh'
+                                      left join fsmonitor.dbo.tfiles r_file on r_file.id = r.tfile_id
+                                    where CAST(f.date AS DATE) = CAST('#{@date}' AS DATE)
+                                            and f.name like '[AB]N10123__________.arj'
+                                    order by f.id asc")
+    client8 = Elodb.new
+    @f311_in = client8.sql.execute("SELECT f.name name
+                                          , CONVERT(char(10), f.time, 108) time
+                                    FROM fsmonitor.dbo.tfiles f
+                                    where CAST(f.date AS DATE) = CAST('#{@date}' AS DATE)
+                                            and (f.name like 'ON10123__________.arj' or f.name like 'S10123________.arj')
+                                    order by f.id asc")
+    client9 = Elodb.new
+    counts_311 = client9.sql.execute("SELECT ISNULL((SELECT count(*) count
+                                  FROM FSMonitor.dbo.tfiles
+                                  where (name like 'S[BKF][EFPR]__3510123[_]____________[_]24900000__________[_]___.XML')
+                                        and CAST(date AS date) = CAST('#{@date}' AS DATE)
+                                  group by date), 0) as count;
+
+                                  SELECT ISNULL((SELECT count(*) count
+                                  FROM FSMonitor.dbo.tfiles
+                                  where (name like 'S[BKF]C__3510123[_]____________[_]24900000__________[_]___.XML')
+                                        and CAST(date AS DATE) = CAST('#{@date}' AS DATE)
+                                  group by date), 0) as count;
+
+                                  SELECT ISNULL((SELECT count(*) count
+                                  FROM FSMonitor.dbo.tfiles
+                                  where ( name like 'ON10123__________.arj' or name like 'S10123________.arj' )
+                                        and CAST(date AS DATE) = CAST('#{@date}' AS DATE)
+                                  group by date), 0) as count;
+
+                                  SELECT ISNULL((SELECT count(*) count
+                                  FROM FSMonitor.dbo.tfiles
+                                  where name like '[AB]N10123__________.arj'
+                                        and CAST(date AS DATE) = CAST('#{@date}' AS DATE)
+                                  group by date), 0) as count;
+
+                                  SELECT ISNULL((SELECT count(*) count
+                                  FROM elodb.dbo.elo_arh_post f
+                                  where (f.filetype = N'ИЭС1' or f.filetype = N'ИЭС2') and f.filename like '0z____10.123%' and f.posttype = '0z' and CAST(f.dt AS DATE) = CAST('#{@date}' AS DATE)
+                                  group by f.posttype), 0) as count;
+
+                                  SELECT 0 as count;
+
+                                  SELECT ISNULL((SELECT count(*) count
+                                  FROM elodb.dbo.elo_arh_post f
+                                  where f.filetype = N'ОЭС' and f.filename like '0z___123.010%' and f.posttype = '0z' and CAST(f.dt AS DATE) = CAST('#{@date}' AS DATE)
+                                  group by f.filetype), 0) as count;")
+    @f311_count_in,
+    @f311_count_out,
+    @f311_count_arj_in,
+    @f311_count_arj_out,
+    @f311_count_ptkpsd_in,
+    @f311_count_ptkpsd_in_error,
+    @f311_count_ptkpsd_out =
+        counts_311.collect { |c| c["count"] }
 
     @f440_count_in,
     @f440_count_out,
@@ -164,6 +227,8 @@ class HomeController < ApplicationController
         counts_550.collect { |c| c["count"] }
 
     cmd = "call c:\\utils\\files_count_in_archive.cmd #{@date}"
+    @f311_count_arj_system_in,
+    @f311_count_arj_system_out,
     @f440_count_arj_system_in,
     @f440_count_arj_system_out,
     @f550_count_arj_system_in,
