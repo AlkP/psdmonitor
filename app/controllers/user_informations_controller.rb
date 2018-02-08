199d0f4c6959bf56d97e16256f294f68a5c96c0b
Alexey V. Pasenko <apasenko@gmail.com>
2018-02-08 12:30:46 +0300

    fix показ пользователя

--- app/controllers/user_informations_controller.rb
+++ app/controllers/user_informations_controller.rb
@@ -8,6 +8,5 @@ class UserInformationsController < ApplicationController
     @name = user_information.where("CODE = '$exec$'")[0].nil? ? "" : user_information.where("CODE = '$exec$'")[0].VOL
     @email = user_information.where("CODE = '$execmail$'")[0].nil? ? "" : user_information.where("CODE = '$execmail$'")[0].VOL
     @elo_usr_protocol = EloUsrProtocol.where("USERID = ?", params[:id])
-    # @elo_usr_errs = EloUsrErr.where("USERID = ?", params[:id])
   end
 end
